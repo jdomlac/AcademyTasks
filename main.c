fdd28c34950e91cef8d616e3de9b8f5657c1ce26
Juan Domínguez Lacal <jdomlac@etsinf.upv.es>
2020-11-30 17:32:56 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+void initVars(){
+
+uint a=24,b=6,c=8;
+float result;
+
+}
+
+void main(){
+
+	initVars();
+	while(1){
+		result=a/b;
+		printf("The result is: %f.2\n",&result);
+	}
+}
+
