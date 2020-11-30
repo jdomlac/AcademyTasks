532cb55153fa7159d334224d334bca65df9a06cc
Juan Domínguez Lacal <jdomlac@etsinf.upv.es>
2020-11-30 18:10:45 +0100

    Task 1 commit

--- main.c
+++ main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 
-void initVars(){
+void initVars(){	//This function will initialize the variables we'll use
 
-uint a=24,b=6,c=8;
-float result;
+uint a=24,b=6,c=8;	//Declaration of the integers a, b and c with their respective values
+float result;		//Declaration of an empty float, that will be used later
 
 }
 
 void main(){
 
-	initVars();
-	while(1){
-		result=a/b;
+	initVars();		//Calling the function initVars so we can use our variables
+	while(1){		//Start of a while loop, with an always true condition
+		result=a/c;	//We asign the value of a divided by c to the variable result
+		
+		//Output "The result is: " followed by the float result with two decimals after floating point
 		printf("The result is: %f.2\n",&result);
 	}
 }
